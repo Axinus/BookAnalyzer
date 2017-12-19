1f224bcdddac95303a120fd7e0bc89ac4b5a8a37
Mateusz Fec <mateuszfec@gmail.com>
2017-12-19 22:48:21 +0100

    Otwarcie dowolnego pliku, wczytywanie linii tekstu do bufora (100) elementowego, usuwanie pustych przestrzeni z bufora, zmiana rozmiaru bufora po przepelnieniu

--- BookAnalyzer/BookAnalyzer.c
+++ BookAnalyzer/BookAnalyzer.c
@@ -1,31 +1,27 @@
 // ------------------------------------- Biblioteki -------------------------------------
 #include <stdio.h>
-
+#include <stdlib.h>
 // --------------------------------------- STALE ----------------------------------------
 
 
 // ---------------------------------- Naglowki funkcji ----------------------------------
+char ** loadfile(char * filename, int *len);
 
 
 // ------------------------------------- Biblioteki -------------------------------------
 int main(int argc, char *argv[])
 {
-	
-
-	int num;
-	FILE *fptr;
-	char c;
-
-	if ((fptr = fopen("testowy_tekst.txt", "r")) == NULL) {
-		printf("Error! opening file");
+	if (argc == 1) {
+		fprintf(stderr, "Wprowadz nazwe pliku do odczytu");
 		exit(1);
 	}
 
-	while ((c = fgetc(&fptr)) != EOF) {
-		printf("%c", c);
-	}
+	// Load file into data structure
+	int length = 0;
+	char **words = loadfile(argv[1], &length);
 
-	fclose(fptr);
+	// Display the first 100 lines
+
 
 
 	getchar();
@@ -33,3 +29,47 @@ int main(int argc, char *argv[])
 	return 0;
 }
 // -------------------------------------- Funkcje ---------------------------------------
+char ** loadfile(char *filename, int *len) {
+	// Otwarcie pliku
+	FILE *f = fopen(filename, "r");
+	if (!f) {
+		fprintf(stderr, "Nie moge otworzyc pliku");
+		return NULL;
+	}
+
+
+	// Alokacja miejsca dla 100 wskaznikow
+	// tutaj sa linie, ale my chcemy docelowo zdania
+	char ** lines = (char **)malloc(100 * sizeof(char *)); // Tutaj wedlug Dudy pierwszy element tablicy a nie typ zmiennej
+
+														   // Bufor dla pojedynczego slowa
+	char buf[1000];
+
+	int i = 0;
+	// Zczytanie slow
+	while (fgets(buf, 1000, f))
+	{
+		if (i == 100) {
+			realloc(lines, 200 * sizeof(char *));
+		}
+
+
+		// Trim of newline char // nieobowiazkowe !!! SPRAWDZIC
+		buf[strlen(buf)] = '\0';
+
+		// Get length of buf
+		int slen = strlen(buf);
+
+		// Zalokowanie miejsca dla stringa (odpowiednio dlugiego)
+		// Nowy rozmiar stringa // po ucieciu zbednego miejsca
+		char *str = (char *)malloc(slen * sizeof(char));
+
+		// Kopiuj string z bufora do stringa docelowego
+		strcpy(str, buf);
+
+		// Dolacz string docelowy do struktury zaczytanych danych
+		lines[i] = str;
+
+		i++;
+	}
+}
